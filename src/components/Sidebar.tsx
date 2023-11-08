17c69367a73c0bbdf9149f9142144978c1e20c00
kumarnabin <touchnabin@gmail.com>
2023-11-08 20:25:42 +0545

    sub menu added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLineChart as faChart, faCog, faUser} from '@fortawesome/free-solid-svg-icons';
+import {faCog, faLineChart as faChart, faUser} from '@fortawesome/free-solid-svg-icons';
+
+const MenuItem = ({label, icon, to, subMenu}) => {
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+
+    const toggleSubMenu = () => {
+        setIsSubMenuOpen(!isSubMenuOpen);
+    };
+
+    return (
+        <div>
+            {subMenu ? (
+                <div className="menu-item">
+                    <div className="link" onClick={toggleSubMenu}>
+                        <FontAwesomeIcon icon={icon}/>
+                        <span className="menu-text">{label}</span>
+                    </div>
+                    <div className={`sub-menu ${isSubMenuOpen ? '' : 'close'}`}>
+                        <div>
+                            {subMenu.map((subItem, subIndex) => (
+                                <div>
+                                    <NavLink to={subItem.to} key={subIndex} className="link">
+                                        <span className="menu-text">{subItem.label}</span>
+                                    </NavLink>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ) : (<div className="menu-item">
+                    <NavLink to={to} className="link">
+                        <FontAwesomeIcon icon={icon}/>
+                        <span className="menu-text">{label}</span>
+                    </NavLink>
+                </div>
+            )}
+        </div>
+    );
+};
 
 const menuItems = [
-    { label: 'Dashboard', icon: faChart, to: '/dashboard' },
-    { label: 'Profile', icon: faUser, to: '/profile' },
-    { label: 'Settings', icon: faCog, to: '/settings' },
+    {label: 'Dashboard', icon: faChart, to: '/dashboard'},
+    {label: 'Profile', icon: faUser, to: '/profile'},
+    {
+        label: 'Settings',
+        icon: faCog,
+        subMenu: [
+            {label: 'General', to: '/settings/general'},
+            {label: 'Security', to: '/settings/security'},
+        ],
+    },
 ];
 
-
-function Navbar() {
-
+function Sidebar() {
     return (
         <div className="sidebar">
             {menuItems.map((menuItem, index) => (
-                <div className="menu-item" key={index}>
-                    <NavLink to={menuItem.to} className="link">
-                        <FontAwesomeIcon icon={menuItem.icon}/>
-                        <span className="menu-text">{menuItem.label}</span>
-                    </NavLink>
-                </div>
+                <MenuItem
+                    key={index}
+                    label={menuItem.label}
+                    icon={menuItem.icon}
+                    to={menuItem.to}
+                    subMenu={menuItem?.subMenu}
+                />
             ))}
-        </div>);
+        </div>
+    );
 }
 
-export default Navbar;
+export default Sidebar;
