2014fed6fc550cb19b9e3d17d2a73d17807674a7
kumarnabin <touchnabin@gmail.com>
2023-11-17 10:36:51 +0545

    signal used for form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,72 +1,45 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {computed, signal} from "@preact/signals-react";
+
+const emailSignal = signal('');
+const passwordSignal = signal('');
+const isValidEmail = (email) => {
+    const regex = /^[\w-.]+@([\w-]+.)+[a-z]{2,}$/;
+    return regex.test(email);
+};
+const isValidPassword = (password) => {
+    const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*])[^\s]{8,}$/;
+    return strongPasswordRegex.test(password);
+};
+
+const isEmailValid = computed(() => isValidEmail(emailSignal.value));
+const isPasswordValid = computed(() => isValidPassword(passwordSignal.value));
 
 function Login() {
-    const [document, setDocument] = useState({name: '', file: null, date: ''});
-    const [errors, setErrors] = useState({name: '', file: '', date: ''});
 
-    const handleChange = (e) => {
-        const {name, value, type, files} = e.target;
-
-        if (type === 'file') {
-            if (files.length > 0) {
-                setDocument({...document, [name]: files[0]});
-                setErrors({...errors, file: ''});
-            } else {
-                setDocument({...document, [name]: null});
-                setErrors({...errors, file: 'File is required'});
-            }
-        } else {
-
-            setDocument({...document, [name]: value});
-            if (name === 'name') {
-
-                if (value.trim() === '') {
-                    setErrors({...errors, name: 'Invalid name'});
-                } else {
-                    setErrors({...errors, name: ''});
-                }
-            }
-        }
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if (!document.file) {
-            setErrors({...errors, file: 'File is required'});
-        }
-
-        if (document.name.trim() === '') {
-            setErrors({...errors, name: 'Invalid name'});
-        }
-
-        console.log('File: ', document.file);
-    };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="name"
-                value={document.name}
-                onChange={handleChange}
-            />
-            {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
-            <input
-                type="file"
-                name="file"
-                onChange={handleChange}
-            />
-            {errors.file && <p style={{color: 'red'}}>{errors.file}</p>}
+        <form>
+            <div>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    value={emailSignal.value}
+                    onChange={(event) => emailSignal.value = event.target.value}
+                />
+                {isEmailValid.value ? null : <p style={{color: "red"}}>Invalid email</p>}
+            </div>
+            <div>
 
-            <input
-                type="date"
-                name="date"
-                value={document.date}
-                onChange={handleChange}
-            />
-            {errors.name && <p style={{color: 'red'}}>{errors.name}</p>}
-            <button type="submit">Submit</button>
+                <label>Password:</label>
+                <input
+                    type="password"
+                    value={passwordSignal.value}
+                    onChange={(event) => passwordSignal.value = event.target.value}
+                />
+                {isPasswordValid.value ? null : <p style={{color: "red"}}>Invalid password</p>}
+            </div>
+            <button disabled={!isEmailValid.value && !isPasswordValid.value}>Submit</button>
         </form>
     );
 }
